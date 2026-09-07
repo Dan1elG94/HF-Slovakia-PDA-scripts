02a85780836afa09f8507cad19dc7f85aeb7d762
Dan1elG94 <101414693+Dan1elG94@users.noreply.github.com>
2026-09-07 09:50:48 +0200

    update

--- enhanced-header.user.js
+++ enhanced-header.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PDA - Enhanced Header
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.4
 // @updateURL    https://github.com/Dan1elG94/HF-Slovakia-PDA-scripts/raw/refs/heads/main/enhanced-header.user.js
 // @downloadURL  https://github.com/Dan1elG94/HF-Slovakia-PDA-scripts/raw/refs/heads/main/enhanced-header.user.js
 // @description  Bigger name, added text to buton and highlighted logout button
@@ -17,7 +17,7 @@
   const USERNAME_SUFFIX = 'Label_Username-bdi';
   const USERNAME_FONT_SIZE = '1.8rem';
   const USERNAME_FONT_WEIGHT = 'bold';
-  const USERNAME_FONT_COLOR = '#ff0000';
+  const USERNAME_FONT_COLOR = '#1f1f1f';
   const BUTTON_TEXT_MARGIN_RIGHT = '0.6rem';
 
   const BUTTONS = [
